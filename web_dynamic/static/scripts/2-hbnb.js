6073e564f3251cd7a64c4dfe9f9c8401ce94eede
diego0096 <dfqz93@hotmail.com>
2020-05-15 22:02:51 +0000

    new api request

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const amenities = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+    });
+    if (Object.values(amenities).length > 0) {
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+    if (textStatus === 'success') {
+      $('div#api_status').addClass('avaible');
+    } else {
+      $('div#api_status').removeClass('avaible');
+    }
+  });
+});
