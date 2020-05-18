5fd88036520f5ee7de8fd7449b17cac31e3c68a6
diego0096 <dfqz93@hotmail.com>
2020-05-18 17:33:10 +0000

    Add place

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -45,3 +45,16 @@ $.ajax({
 
 });
 
+$.ajax({
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search/',
+  contentType: 'application/json',
+  dataType: 'json',
+  data: JSON.stringify({ amenities: Object.keys(amenities) }),
+  success: function (data) {
+    $('article').remove();
+        for (const place of Object.values(data)) {
+          $('section.places').append('<article><div class="title"><h2>' + place.name + '</h2><div class="price_by_night">' + place.price_by_night + '</div></div><div class="information">' + '<div class="max_guest">' + '<i class="fa fa-users fa-3x" aria-hidden="true"></i><br />' + place.max_guest + ' Guests</div>' + '<div class="number_rooms">' + '<i class="fa fa-bed fa-3x" aria-hidden="true"></i><br />' + place.number_rooms + ' Bedrooms</div>' + '<div class="number_bathrooms">' + '<i class="fa fa-bath fa-3x" aria-hidden="true"></i><br />' + place.number_bathrooms + 'Bathroom</div>' + '</div><div class="user"><strong>Owner: ' + ${users[place.user_id]}</strong></div> + '<div class="description">' + place.description + '</div></article>');
+        }
+        for (const i = 0; i < data.length; i++) {
+          const place = data[i];
